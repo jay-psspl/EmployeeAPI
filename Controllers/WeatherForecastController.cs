ea90753bf843d8dacb08e44f0038004c480b2cd3
jay-psspl <jay@prakashinfotech.com>
2022-04-25 10:16:29 +0530

    #1 Change City Name

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace EmployeeAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "New York"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
